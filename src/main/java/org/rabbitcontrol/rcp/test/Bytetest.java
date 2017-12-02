4b23d828e4a72ea6eb6c0ce1ccd3b1bac24a67ca
Ingo Randolf <ingo.randolf@servus.at>
2017-12-02 14:29:22 +0100

    add enum-type to test

--- src/main/java/org/rabbitcontrol/rcp/test/Bytetest.java
+++ src/main/java/org/rabbitcontrol/rcp/test/Bytetest.java
@@ -1,7 +1,10 @@
 package org.rabbitcontrol.rcp.test;
 
-import org.rabbitcontrol.rcp.model.ParameterFactory;
-import org.rabbitcontrol.rcp.model.interfaces.INumberParameter;
+import io.kaitai.struct.KaitaiStream;
+import org.rabbitcontrol.rcp.model.Parameter;
+import org.rabbitcontrol.rcp.model.exceptions.RCPDataErrorException;
+import org.rabbitcontrol.rcp.model.exceptions.RCPUnsupportedFeatureException;
+import org.rabbitcontrol.rcp.model.parameter.EnumParameter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -15,9 +18,20 @@ public class Bytetest {
 
         ByteArrayOutputStream os = new ByteArrayOutputStream();
 
-        long id = Long.MAX_VALUE;//;(long)(Math.pow(2, 32) - 1);
-        final INumberParameter<Integer> parameter = ParameterFactory.createNumberParameter(12,
-                                                                                           Integer.class);
+//        long id = Long.MAX_VALUE;//;(long)(Math.pow(2, 32) - 1);
+//        final INumberParameter<Integer> parameter = ParameterFactory.createNumberParameter(12,
+//                                                                                           Integer.class);
+
+
+        final EnumParameter parameter = new EnumParameter(111);
+        parameter.getEnumTypeDefinition().addEntry("uno");
+        parameter.getEnumTypeDefinition().addEntry("dos");
+        parameter.getEnumTypeDefinition().addEntry("tres");
+        parameter.getEnumTypeDefinition().addEntry("quattro");
+        parameter.getEnumTypeDefinition().setDefault(3);
+        parameter.setValue(2);
+        parameter.setLabel("a enum");
+        parameter.setDescription("enum description");
 
 
         try {
@@ -28,6 +42,21 @@ public class Bytetest {
             byte[] the_bytes = os.toByteArray();
 
             System.out.println("generate packet:\n" + bytesToHex(the_bytes));
+
+
+
+
+            // parse
+            try {
+                final Parameter param = Parameter.parse(new KaitaiStream(the_bytes));
+                param.dump();
+            }
+            catch (RCPUnsupportedFeatureException _e) {
+                _e.printStackTrace();
+            }
+            catch (RCPDataErrorException _e) {
+                _e.printStackTrace();
+            }
 
         }
         catch (IOException _e) {
