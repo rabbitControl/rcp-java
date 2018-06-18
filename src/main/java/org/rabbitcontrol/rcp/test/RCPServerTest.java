d112e640aa3af2df828e31193743fbb5dac67682
Ingo Randolf <ingo.randolf@servus.at>
2018-06-18 13:01:12 +0200

    cleanup servertest

--- src/main/java/org/rabbitcontrol/rcp/test/RCPServerTest.java
+++ src/main/java/org/rabbitcontrol/rcp/test/RCPServerTest.java
@@ -1,11 +1,15 @@
 package org.rabbitcontrol.rcp.test;
 
-import org.rabbitcontrol.rcp.model.RCPCommands;
+import org.rabbitcontrol.rcp.model.RCPCommands.Init;
+import org.rabbitcontrol.rcp.model.RCPCommands.Update;
+import org.rabbitcontrol.rcp.model.RcpTypes.Datatype;
 import org.rabbitcontrol.rcp.model.Widget;
 import org.rabbitcontrol.rcp.model.exceptions.RCPParameterException;
 import org.rabbitcontrol.rcp.model.interfaces.INumberParameter;
 import org.rabbitcontrol.rcp.model.interfaces.IParameter;
 import org.rabbitcontrol.rcp.model.parameter.*;
+import org.rabbitcontrol.rcp.model.types.Range;
+import org.rabbitcontrol.rcp.model.widgets.CustomWidget;
 import org.rabbitcontrol.rcp.model.widgets.TextboxWidget;
 import org.rabbitcontrol.rcp.test.websocket.server.WebsocketServerTransporterNetty;
 import org.rabbitcontrol.rcp.transport.RCPServer;
@@ -13,8 +17,9 @@ import org.rabbitcontrol.rcp.transport.RCPServer;
 import java.awt.*;
 import java.io.IOException;
 import java.security.cert.CertificateException;
+import java.util.UUID;
 
-public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
+public class RCPServerTest implements Update, Init {
 
     public static boolean doAutoUpdate = true;
 
@@ -34,7 +39,7 @@ public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
 
             if (doAutoUpdate) {
 
-                Thread t = new Thread(new Runnable() {
+                final Thread t = new Thread(new Runnable() {
 
                     @Override
                     public void run() {
@@ -48,25 +53,13 @@ public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
                             catch (final InterruptedException _e) {
                                 break;
                             }
-                            catch (RCPParameterException _e) {
-                                _e.printStackTrace();
-                            }
                         }
                     }
                 });
                 t.start();
             }
         }
-        catch (final InterruptedException _e) {
-            _e.printStackTrace();
-        }
-        catch (final CertificateException _e) {
-            _e.printStackTrace();
-        }
-        catch (final IOException _e) {
-            _e.printStackTrace();
-        }
-        catch (RCPParameterException _e) {
+        catch (final RCPParameterException _e) {
             _e.printStackTrace();
         }
     }
@@ -79,7 +72,7 @@ public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
 
     private INumberParameter<Double> theValueDouble;
 
-    private INumberParameter<Float> theValueFloat;
+    private INumberParameter<Float> theValueFloat1;
 
     private INumberParameter<Float> theValueFloat2;
 
@@ -103,11 +96,7 @@ public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
 
     //------------------------------------------------------------
     //
-    public RCPServerTest() throws
-                           IOException,
-                           CertificateException,
-                           InterruptedException,
-                           RCPParameterException {
+    public RCPServerTest() throws RCPParameterException {
 
         // a udp transporter
         //        final UDPServerTransporter transporter = new UDPServerTransporter(8181);
@@ -118,86 +107,116 @@ public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
 
         // a websocket transporter
         final WebsocketServerTransporterNetty transporter = new WebsocketServerTransporterNetty();
+        final WebsocketServerTransporterNetty transporter2 = new WebsocketServerTransporterNetty();
 
         // create rabbit
         rabbit = new RCPServer(transporter);
-        //rabbit.addTransporter(transporter);
+        rabbit.addTransporter(transporter2);
 
         rabbit.setUpdateListener(this);
         rabbit.setInitListener(this);
 
         transporter.bind(10000);
+        transporter2.bind(12000);
 
         //------------------------------------------------------------
         //------------------------------------------------------------
+        // expose parameters
 
-        //        final ArrayParameter<Byte[][][], Byte> arr = rabbit.createArrayParameter
-        // ("byte-array " +
-        //                                                                                 "3-2-2",
-        //
-        // RcpTypes.Datatype.INT8,
-        //                                                                                 4,
-        //                                                                                 2,
-        //                                                                                 2);
-        //
-        //         arr.getElementType().setDefault((byte)123);
-        //
-        //
-        //
-        //        arr.setValue(new Byte[][][] { { { 1, 2 }, { 3, 4 } },
-        //                                      { { 11, 12 }, { 13, 14 } },
-        //                                      { { 21, 22 }, { 23, 24 } },
-        //                                      { { 31, 32 }, { 33, 34 } }});
-        //
-        //        arr.getValue()[0][0][0] = 111;
-        //
-        //        final ArrayParameter<Double[], Double> arr1 = rabbit.createArrayParameter
-        // ("double array",
-        //                                    RcpTypes.Datatype.FLOAT64, 6);
-        //
-        //        //arr1.getArrayDefinitionFixed().setDefault(new Double[] { 1.1, 2.2, 3.3, 4.4,
-        // 5.5, 6.6 });
-        //
-        //        arr1.setValue(new Double[] { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 });
-        //
-        //
-        //        final ArrayParameter<Integer[][], Integer> arr2 = rabbit.createArrayParameter
-        // ("int 2D",
-        //                                    RcpTypes.Datatype.INT32,
-        //                                    3,
-        //                                    2);
-        //
-        //        arr2.setValue(new Integer[][] { { 1024, 2048 },
-        //                                        { 3333, 6666 },
-        //                                        { 10000, 10001 } });
-        //
-        //
-        //        final ArrayParameter<String[], String> arr3 = rabbit.createArrayParameter
-        // ("String " +
-        //
-        // "Array",
-        //                                    RcpTypes.Datatype.STRING,
-        //                                    3);
-        //
-        //        arr3.setValue(new String[] { "test", "string", "array" });
-        //
-        //
-        //
-        //
-        //        final ArrayParameter<List<List<List<Byte>>>, Byte> list = rabbit
-        // .createArrayParameter("byte-list",
-        //
-        //         RcpTypes.Datatype.INT8, 3);
+        exposeRange();
+//        exposeArray();
+//        exposeList();
+//        exposeGroupWithCustomWidget();
+//        exposeLong();
+//        exposeParameterInGroups();
+//        exposeWithWidget();
 
-        //        list.setMinDimSizes(3, 0, 2);
-        //        list.setMaxDimSizes(10, -1, 2);
-        //
-        //
-        //
-        //        RangeParameter<Integer> range = rabbit.createRangeParameter<Integer>("");
+        rabbit.update();
+    }
+
+
+    private void exposeRange() throws RCPParameterException {
+        final RangeParameter<Byte> rangeParameter = rabbit.createRangeParameter("range",
+                                                                                Byte.class);
+
+        rangeParameter.setValue(new Range<Byte>((byte)2, (byte)4));
+        rangeParameter.getTypeDefinition().setDefault(new Range<Byte>((byte)1, (byte)10));
+        rangeParameter.getRangeDefinition().getElementType().setMinimum((byte)0);
+        rangeParameter.getRangeDefinition().getElementType().setMaximum((byte)20);
+    }
+
+    private void exposeArray() throws RCPParameterException {
+
+        //--------------------------------------------
+        // expose 1 dimensions array of strings with 3 elements
+        final ArrayParameter<String[], String> arr3 = rabbit.createArrayParameter("String " +
+
+                                                                                  "Array",
+                                                                                  Datatype.STRING,
+                                                                                  3);
+        arr3.setValue(new String[]{ "test", "string", "array" });
+
+
+        //--------------------------------------------
+        // expose one-dimensional array of doubles with 6 elements
+        final ArrayParameter<Double[], Double> arr1 = rabbit.createArrayParameter("double array",
+                                                                                  Datatype.FLOAT64,
+                                                                                  6);
+
+        arr1.getArrayDefinition().setDefault(new Double[] { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 });
+        arr1.setValue(new Double[]{ 10.1, 20.2, 30.3, 40.4, 50.5, 60.6 });
+
+
+        //--------------------------------------------
+        // expose 2-dimensional array of integer: 3, 2
+        final ArrayParameter<Integer[][], Integer> arr2 = rabbit.createArrayParameter("int 2D",
+                                                                                      Datatype.INT32,
+                                                                                      3,
+                                                                                      2);
+
+        arr2.setValue(new Integer[][]{ {1024, 2048},
+                                       {3333, 6666},
+                                       {10000, 10001}});
+
+
+        //--------------------------------------------
+        // expose array with 3 dimensions: 4, 2, 2
+        final ArrayParameter<Byte[][][], Byte> arr = rabbit.createArrayParameter("byte-array " +
+                                                                                 "3-2-2",
+                                                                                 Datatype.INT8,
+                                                                                 4,
+                                                                                 2,
+                                                                                 2);
+
+        // set default value of element type
+        arr.getElementType().setDefault((byte)123);
+
+        // set value
+        arr.setValue(new Byte[][][] { { { 1, 2 }, { 3, 4 } },
+                                      { { 11, 12 }, { 13, 14 } },
+                                      { { 21, 22 }, { 23, 24 } },
+                                      { { 31, 32 }, { 33, 34 } } });
+
+        // change one element in the array
+        arr.getValue()[0][0][0] = 111;
+    }
+
+    private void exposeList() {
+
+//        final ArrayParameter<List<List<List<Byte>>>, Byte> list = rabbit.createArrayParameter(
+//                "byte-list",
+//
+//                Datatype.INT8,
+//                3);
+//
+//        list.setMinDimSizes(3, 0, 2) list.setMaxDimSizes(10, -1, 2)
+
+    }
+
+    private void exposeWithWidget() throws RCPParameterException {
 
         // string
-        theValueString = rabbit.createStringParameter("text label");
+        theValueString = rabbit.createStringParameter("string with widget");
         theValueString.setValue("This is a text encoded in utf-8. let's test it:");
 
         theValueString.setDescription("test description 2");
@@ -211,13 +230,41 @@ public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
         theValueString.setLanguageDescription("deu", "deutsche beschreibung");
         theValueString.setLanguageDescription("fra", "escription francoise");
 
-        Widget str_widget = new TextboxWidget();
+        theValueString.getTypeDefinition().setDefault("defaultstring");
+
+        final Widget str_widget = new TextboxWidget();
         theValueString.setWidget(str_widget);
+    }
+
+    private void exposeGroupWithCustomWidget() throws RCPParameterException {
+
+        // setup custom widget
+        final CustomWidget custom_group_widget = new CustomWidget();
+        custom_group_widget.setUuid(UUID.fromString("69babd06-72d9-11e8-adc0-fa7ae01bbebc"));
+        custom_group_widget.setConfig("testconfig".getBytes());
 
         // group 1
         groupParam1 = rabbit.createGroupParameter("GROuP");
-        //        groupParam1.setWidgetType((short)123);
-        //        // enum
+        groupParam1.setWidget(custom_group_widget);
+
+        // add parameter to group
+        rabbit.createInt8Parameter("int 1", groupParam1);
+        rabbit.createInt8Parameter("int 2", groupParam1);
+        rabbit.createInt8Parameter("int 3", groupParam1);
+    }
+
+    private void exposeLong() throws RCPParameterException {
+
+        theValueLong = rabbit.createInt64Parameter("a long number");
+        theValueLong.setValue(10L);
+    }
+
+    private void exposeParameterInGroups() throws RCPParameterException {
+
+        // group 1
+        groupParam1 = rabbit.createGroupParameter("GROUP 1");
+
+        // enumeration
         enumParameter = rabbit.createEnumParameter("enum test", groupParam1);
         enumParameter.getEnumTypeDefinition().addEntry("uno");
         enumParameter.getEnumTypeDefinition().addEntry("dos");
@@ -230,7 +277,10 @@ public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
         colorparam = rabbit.createRGBParameter("a color", groupParam1);
         colorparam.setValue(Color.CYAN);
 
-        theValueString.getTypeDefinition().setDefault("defaultstring");
+        // string
+        theValueString = rabbit.createStringParameter("a string");
+        theValueString.setValue("This is a text encoded in utf-8. let's test it:");
+        theValueString.setDescription("description for string");
 
         // double
         theValueDouble = rabbit.createDoubleParameter("a double", groupParam1);
@@ -238,49 +288,41 @@ public class RCPServerTest implements RCPCommands.Update, RCPCommands.Init {
         theValueDouble.getTypeDefinition().setMinimum(0.D);
         theValueDouble.setDescription("double description");
         theValueDouble.setValue(3.14);
-        //
+
         // float
-        theValueFloat = rabbit.createFloatParameter("FLOAT", groupParam1);
-        theValueFloat.setValue(123.F);
+        theValueFloat1 = rabbit.createFloatParameter("FLOAT", groupParam1);
+        theValueFloat1.setValue(123.F);
 
         // float
         theValueFloat2 = rabbit.createFloatParameter("FLOAT 2", groupParam1);
-        theValueFloat2.setValue(123.F);
+        theValueFloat2.setValue(33.F);
 
         // int
-        theValueInt = rabbit.createInt32Parameter("INt LABEL", groupParam1);
+        theValueInt = rabbit.createInt32Parameter("INT32 LABEL", groupParam1);
         theValueInt.setValue(333);
 
         // boolean
         theValueBool = rabbit.createBooleanParameter("toggle button", groupParam1);
         theValueBool.setValue(true);
 
-//        long theValueLong = rabbit.createInt64Parameter("a long number");
-//        theValueLong.setValue((long)10);
+        // group 2 - as subgroup of group1
+        groupParam2 = rabbit.createGroupParameter("foo", groupParam1);
 
-        // group 2
-        groupParam2 = rabbit.createGroupParameter("foo");
+        // group 3 - as subgroup of group1
+        groupParam3 = rabbit.createGroupParameter("bar", groupParam1);
 
-        // group 3
-        groupParam3 = rabbit.createGroupParameter("group 3");
-        //groupParam3.setWidgetType((short)123);
-
-//        groupParam2.addChildren(theValueInt, theValueBool, theValueLong);
-
-        rabbit.addParameter(theValueFloat, groupParam2);
-
-        rabbit.addParameter(theValueLong, groupParam3);
-
-        rabbit.update();
+        // steal move values to other groups
+        rabbit.addParameter(theValueFloat1, groupParam2);
+        rabbit.addParameter(theValueFloat2, groupParam3);
     }
 
     public void updateVar1() {
 
     }
 
-    boolean init = false;
+    boolean init;
 
-    public void updateVar2() throws RCPParameterException {
+    public void updateVar2() {
 
         if (!init) {
 
